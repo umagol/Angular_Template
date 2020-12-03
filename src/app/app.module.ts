b4d2f9364aa8d92f3b3a056cf3aba572bc7861d9
SatishUmagolL <umagol97@gmail.com>
2020-12-03 15:43:58 +0530

    bootstrap, Jquery, and other css, js file downloaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './layout/home/home.component';
+import { ContactComponent } from './layout/contact/contact.component';
+import { AboutMeComponent } from './layout/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
